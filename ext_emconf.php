ad3ed3a103e076b260858707b9a0e2548bc54007
Tobias Liebig <tobias.liebig@typo3.org>
2012-12-12 12:29:37 +0100

    [TASK] update ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,45 +1,41 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "tweet_this".
-#
-# Auto generated 22-12-2009 17:38
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "tweet_this".
+ *
+ * Auto generated 12-12-2012 12:10
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Tweet This',
 	'description' => 'Send tweets from tt_news or t3blog records easily',
 	'category' => 'be',
-	'author' => 'Tobias Liebig',
-	'author_email' => 'liebig@networkteam.com',
-	'shy' => '',
+	'shy' => 0,
+	'version' => '1.4.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => '',
 	'module' => '',
 	'state' => 'beta',
-	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
+	'clearcacheonload' => 0,
 	'lockType' => '',
+	'author' => 'Tobias Liebig',
+	'author_email' => 'mail@etobi.de',
 	'author_company' => '',
-	'version' => '1.1.0',
-	'constraints' => array(
-		'depends' => array(
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"b5ed";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"1364";s:12:"ext_icon.gif";s:4:"0a69";s:14:"ext_tables.php";s:4:"ef87";s:14:"ext_tables.sql";s:4:"6d5f";s:28:"icon_tx_tweetthis_tweets.gif";s:4:"0a69";s:16:"locallang_db.xml";s:4:"e4d3";s:7:"tca.php";s:4:"3172";s:42:"classes/class.tx_tweetthis_AjaxHandler.php";s:4:"5d38";s:37:"classes/class.tx_tweetthis_Helper.php";s:4:"0097";s:40:"classes/class.tx_tweetthis_userField.php";s:4:"ac3a";s:19:"doc/wizard_form.dat";s:4:"c153";s:20:"doc/wizard_form.html";s:4:"5300";s:28:"res/userfield_tweetthis.html";s:4:"e6b0";s:16:"static/setup.txt";s:4:"0b45";}',
-	'suggests' => array(
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => array (
+		'depends' => array (),
+		'conflicts' => array (),
+		'suggests' => array (),
 	),
 );
 
